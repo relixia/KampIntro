c557b3191a087cadc771e6125594405d007398e9
buğra çayır <bugra-net@hotmail.com>
2021-01-29 20:32:40 +0300

    20/01/2021 - Lesson - Day 4

---
+++ Collections/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"Engin", "Buğra", "Dila" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+
+            //isimler = new string[5];
+            //isimler[4] = "İlker";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+                //en son yazdığın Console.WriteLine(isimler[0]); kodu çalışacak ama bir şey yazılmayacak konsola.
+                //sebebi ise isimler döngüsünü içinde 6 veri barındıran yeni bir string yaptın fakat bu stringde sadece 5. elemanı tanımladın, kalan elemanlar boş.
+
+
+            List<string> isimler2 = new List<string> {"Engin", "Buğra", "Dila" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[0]);
+
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, UnitsInStock = 5, ProductName = "Kalem", UnitPrice = 35 };
+
+            //PascalCase        camelCase
+            //case sensitive - büyük küçük harfe duyarlı - C# ve Java
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+            //bu "void" kullanarak yaptığın işlem, sen bunun sonucunda çıkan "9"u başka bir yerde kullanamıyorsun.
+                //productManager.Topla2(3, 6);
+
+            //bu "int" kullanarak yaptığın işlem, void yerine bunu kullanarak sonucu başka yerlerde de kullanabiliyorsun.
+                //int toplamaSonucu = productManager.Topla(3, 6);
+                //Console.WriteLine(toplamaSonucu * 2);
+
+            //int, bool, double - değer tip
+            //class, user interface, diziler - referans tip
+            //ref - out
+
+        }
+    }
+}
